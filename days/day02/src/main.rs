396585ab53373829214b300e87a0eb97fedd8089
Dave Hein <JeNeSuisPasDave@users.noreply.github.com>
2025-12-02 17:52:56 -0600

    (D02) Implemented Part 2, but with a bug
    I implemented Part 2, with a nice bit of code that almost works
    correctly. The remaining problem is that the product ID 222222 is
    matched by a pattern of repeated 2s, 22s, and 222s ... so it shows
    up in the output three times. I'm thinking about solving this with
    a hash collection or similar uniquely keyed collection.

--- days/day02/src/main.rs
+++ days/day02/src/main.rs
@@ -48,7 +48,56 @@ impl IdRange {
         Some(IdRange::new(start, end))
     }
 
-    fn invalid_ids(self) -> Vec<u64> {
+    // This is the Part 2 function that produces invalid IDs
+    // within a range (inclusive)
+    //
+    fn invalid_ids(&self) -> Vec<u64> {
+        let mut result: Vec<u64> = Vec::new();
+
+        // start with a pattern size of a single digit
+        //
+        let mut pattern_mag: u32 = 0;
+        loop {
+            let pattern_inc: u64 = u64::pow(10, pattern_mag) * 10;
+            let pattern_min: u64 = pattern_inc / 10;
+            // if minimum pattern value is larger than self.end
+            // then stop looking at this and larger patterns
+            //
+            if (pattern_min * pattern_inc) > self.end {
+                break;
+            }
+            // loop through the pattern values from pattern_inc - 1
+            // down to pattern_min and capture any repeated
+            // pattern values that are within the IdRange.
+            //
+            let mut pattern_num = pattern_inc;
+            loop {
+                pattern_num -= 1;
+                if pattern_num < pattern_min {
+                    break;
+                }
+                let mut num: u64 =
+                    (pattern_num * pattern_inc) + pattern_num;
+                while num < self.start {
+                    num = (num * pattern_inc) + pattern_num;
+                }
+                while num <= self.end {
+                    result.push(num);
+                    num = (num * pattern_inc) + pattern_num;
+                }
+            }
+
+            // expand the size of the pattern
+            //
+            pattern_mag += 1;
+        }
+        return result;
+    }
+
+    // This is the Part 1 function that produced invalid
+    // ids within a range (inclusive)
+    //
+    fn invalid_ids1(&self) -> Vec<u64> {
         let mut result: Vec<u64> = Vec::new();
         let start_s = self.start.to_string();
         let end_s = self.end.to_string();
@@ -128,14 +177,14 @@ fn main() -> Result<()> {
                 continue;
             }
             let idr = idr.unwrap();
+            for invalid_id in idr.invalid_ids() {
+                println!("Invalid ID: {}", invalid_id);
+                invalid_id_accum += invalid_id;
+            }
             println!(
                 "Range: {}-{}; accum: {}",
                 idr.start, idr.end, invalid_id_accum
             );
-            for invalid_id in idr.invalid_ids() {
-                // println!("Invalid ID: {}", invalid_id);
-                invalid_id_accum += invalid_id;
-            }
         }
     }
     if s.len() > 0 {
@@ -144,14 +193,22 @@ fn main() -> Result<()> {
         let idr = IdRange::new_from_str(&ss);
         if idr.is_some() {
             let idr = idr.unwrap();
-            // println!("Range: {}-{}", idr.start, idr.end);
             for invalid_id in idr.invalid_ids() {
-                // println!("Invalid ID: {}", invalid_id);
+                println!("Invalid ID: {}", invalid_id);
                 invalid_id_accum += invalid_id;
             }
+            println!(
+                "Range: {}-{}; accum: {}",
+                idr.start, idr.end, invalid_id_accum
+            );
         }
     }
     println!("Sum of invalid_ids: {}", invalid_id_accum);
+
+    // dummy lines to avoid 'unused' warning
+    //
+    let x = IdRange::new(1, 2);
+    let _ = x.invalid_ids1();
 
     Ok(())
 }
