07818c1aac30ce831048c4e88ba5e9614d000f76
Dave Hein <JeNeSuisPasDave@users.noreply.github.com>
2025-12-02 10:49:26 -0600

    (D02) Added I/O and parsing into IdRange structs
    I/O supports UTF-8 files. I also found a way to make a 'static'
    RegEx variable, so I don't have to create the RegEx each time
    I want to parse a product ID range string.

--- days/day02/src/main.rs
+++ days/day02/src/main.rs
@@ -1,10 +1,12 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::BufReader;
 use std::path::PathBuf;
+use std::sync::LazyLock;
 
 use anyhow::{Context, Result};
 use clap::Parser;
 use regex::Regex;
+use utf8_chars::BufReadCharsExt;
 
 /// Given input file containing the comma-separated list of product ID
 /// ranges, determine the sum of all invalid product IDs.
@@ -15,6 +17,38 @@ struct Cli {
     path: PathBuf,
 }
 
+#[derive(Debug)]
+struct IdRange {
+    start: u32,
+    end: u32,
+}
+
+static IDRANGE_RE: LazyLock<Regex> = LazyLock::new(|| {
+    Regex::new(r"^\s*([0-9]+)-([0-9]+)\s*,?\s*$").unwrap()
+});
+
+impl IdRange {
+    fn new(start: u32, end: u32) -> Self {
+        Self {
+            start: start,
+            end: end,
+        }
+    }
+
+    fn new_from_str(id_range: &str) -> Option<Self> {
+        if !IDRANGE_RE.is_match(id_range) {
+            println!("*** FAILED *** to match range '{}'", id_range);
+            return None;
+        }
+        let caps = IDRANGE_RE.captures(&id_range).unwrap();
+        let sstr: &str = caps.get(1).unwrap().as_str();
+        let estr: &str = caps.get(2).unwrap().as_str();
+        let start: u32 = sstr.parse::<u32>().unwrap();
+        let end: u32 = estr.parse::<u32>().unwrap();
+        Some(IdRange::new(start, end))
+    }
+}
+
 fn main() -> Result<()> {
     let args = Cli::parse();
     let path = &args.path;
@@ -22,13 +56,30 @@ fn main() -> Result<()> {
     let f = File::open(path).with_context(|| {
         format!("Could not open `{}`", path.display())
     })?;
-    let rdr = BufReader::new(f);
-    let lines = rdr.lines();
-
-    for line in lines {
-        let line = line.with_context(|| {
-            format!("Problem reading from `{}`", path.display())
-        })?;
+    let mut rdr = BufReader::new(f);
+    let mut s = Vec::new();
+    for c in rdr.chars().map(|x| x.unwrap()) {
+        s.push(c);
+        if c == ',' {
+            let ss = s.iter().collect::<String>();
+            s.clear();
+            let idr = IdRange::new_from_str(&ss);
+            if idr.is_none() {
+                continue;
+            }
+            let idr = idr.unwrap();
+            println!("Range: {}-{}", idr.start, idr.end);
+        }
     }
+    if s.len() > 0 {
+        let ss = s.iter().collect::<String>();
+        s.clear();
+        let idr = IdRange::new_from_str(&ss);
+        if idr.is_some() {
+            let idr = idr.unwrap();
+            println!("Range: {}-{}", idr.start, idr.end);
+        }
+    }
+
     Ok(())
 }
