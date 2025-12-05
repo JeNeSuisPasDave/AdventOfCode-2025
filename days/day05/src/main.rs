4eed25859de4ad6c673d9b084517d012e1d4565e
Dave Hein <JeNeSuisPasDave@users.noreply.github.com>
2025-12-05 08:59:02 -0600

    (D05) Implemented handling of overlapping ranges
    Refactored the implementation to handle ranges
    that are overlapping. No longer on relying on an
    ID to be in 1 or 0 ranges; can now belong to
    multiple ranges.
    
    Also ignoring any 'trash' folders.

--- days/day05/src/main.rs
+++ days/day05/src/main.rs
@@ -1,7 +1,7 @@
 use std::collections::BTreeMap;
-use std::fmt;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::ops::Bound::{Included, Unbounded};
 use std::path::PathBuf;
 
 use anyhow::{Context, Result};
@@ -17,6 +17,128 @@ struct Cli {
     path: PathBuf,
 }
 
+// models a range of ingredient IDs
+//
+#[derive(Debug)]
+struct IngredientRange {
+    start: u64,
+    end: u64,
+}
+
+// functions associated with struct IngredientRange
+//
+impl IngredientRange {
+    // constructor
+    //
+    fn new(start: u64, end: u64) -> Self {
+        if start > end {
+            panic!(
+                "start of range must by <= end, but found {} > {}",
+                start, end
+            );
+        }
+        IngredientRange {
+            start: start,
+            end: end,
+        }
+    }
+
+    // Returns true if id is within the range; otherwise false
+    //
+    fn contains(&self, id: u64) -> bool {
+        if id < self.start {
+            false
+        } else if id > self.end {
+            false
+        } else {
+            true
+        }
+    }
+}
+
+// models an ingredient database
+//
+struct IngredientDB {
+    // A list of ingredient ranges in the order added
+    //
+    ranges: Vec<IngredientRange>,
+    // A list of ingredient ranges sorted by starting ID
+    //
+    fresh_ranges_by_start: BTreeMap<u64, usize>,
+    // A list of ingredient ranges sorted by ending ID
+    //
+    fresh_ranges_by_end: BTreeMap<u64, usize>,
+}
+
+// functions associated with IngredientDB
+//
+impl IngredientDB {
+    // constructor
+    //
+    fn new() -> Self {
+        let list: Vec<IngredientRange> = Vec::new();
+        let dict_by_start: BTreeMap<u64, usize> = BTreeMap::new();
+        let dict_by_end: BTreeMap<u64, usize> = BTreeMap::new();
+        IngredientDB {
+            ranges: list,
+            fresh_ranges_by_start: dict_by_start,
+            fresh_ranges_by_end: dict_by_end,
+        }
+    }
+
+    // add a new fresh ingredient range
+    //
+    fn add_range(&mut self, start: u64, end: u64) {
+        let ir = IngredientRange::new(start, end);
+        self.ranges.push(ir);
+        let idx: usize = self.ranges.len().try_into().unwrap();
+        let range_idx: usize = idx - 1;
+        self.fresh_ranges_by_start.insert(start, range_idx);
+        self.fresh_ranges_by_end.insert(end, range_idx);
+    }
+
+    // check whether the ingredient is known to be fresh
+    //
+    fn is_fresh(&self, id: u64) -> bool {
+        println!("Checking freshness of {}", id);
+        let mut result: bool = false;
+        // iterator for all ranges that start at or below the id
+        //
+        let up_bounds = (Unbounded, Included(id));
+        let up_from = self.fresh_ranges_by_start.range(up_bounds);
+        //
+        // iterator for all ranges that end at or above the id
+        //
+        let down_bounds = (Included(id), Unbounded);
+        let down_to = self.fresh_ranges_by_start.range(down_bounds);
+        //
+        //
+        for thing in up_from {
+            let (_, range_idx): (&u64, &usize) = thing;
+            println!("Checking up_from at idx {0}", range_idx);
+            let val: &IngredientRange =
+                self.ranges.get(*range_idx).unwrap();
+            if val.contains(id) {
+                result = true;
+                break;
+            }
+        }
+        if !result {
+            for thing in down_to {
+                let (_, range_idx): (&u64, &usize) = thing;
+                println!("Checking down_to at idx {0}", range_idx);
+                let val: &IngredientRange =
+                    self.ranges.get(*range_idx).unwrap();
+                if val.contains(id) {
+                    result = true;
+                    break;
+                }
+            }
+        }
+        result
+    }
+}
+
 // Binary crate entry point
 //
 fn main() -> Result<()> {
@@ -29,12 +151,43 @@ fn main() -> Result<()> {
     let rdr = BufReader::new(f);
     let lines = rdr.lines();
 
-    let fresh_ingredient_count: u64 = 0;
+    // populate the DB with fresh ingredient ranges. To do
+    // that, read in the ranges until a blank line is encountered
+    //
+    let mut fresh_ingredient_count: u64 = 0;
+    let mut db = IngredientDB::new();
+    let mut process_ids: bool = false;
+    for line in lines {
+        let line = line.with_context(|| {
+            format!("Problem reading from `{}`", path.display())
+        })?;
+        let line = line.trim();
+        if 0 == line.len() {
+            process_ids = true;
+            continue;
+        }
+        if !process_ids {
+            // process ranges
+            //
+            let parts: Vec<&str> = line.split('-').collect();
+            let start: u64 = parts.get(0).unwrap().parse().unwrap();
+            let end: u64 = parts.get(1).unwrap().parse().unwrap();
+            db.add_range(start, end);
+        } else {
+            let id: u64 = line.parse().unwrap();
+            if db.is_fresh(id) {
+                fresh_ingredient_count += 1;
+                println!("FRESH: {}", id);
+            } else {
+                println!("spoiled: {}", id);
+            }
+        }
+    }
 
     // Display the total number of fresh ingredients
     //
     println!(
-        "The count of fresh ingredients is {}",
+        "Thecount of fresh ingredients is {}",
         fresh_ingredient_count
     );
     Ok(())
